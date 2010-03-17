89a7fd81ddecd4bf8f2e8eebc38a64f31661649c
gsm.chenzhiyuan <gsm.chenzhiyuan@gmail.com>
2010-03-17 10:29:54 +0000

    update vectors for aes

--- EpubJava/src/com/ydk/epub/Encrypter.java
+++ EpubJava/src/com/ydk/epub/Encrypter.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.security.spec.AlgorithmParameterSpec;
+import java.util.logging.Logger;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
@@ -18,43 +19,88 @@ import sun.misc.BASE64Encoder;
 
 
 public class Encrypter {
+	private static final Logger log = Logger.getLogger(SearchEngine.class.getName());
+	
 	//given key is HEX 
 	//data is utf-8 BASE64 encoded;
 	//then it is AES 256 encrypted;
 	private Cipher ecipher;
 	private Cipher dcipher;
-	private byte[] _key = new byte[16]; //256bits AES key is not support by java by default 
+	private byte[] _key;// = new byte[16]; //256bits AES key is not support by java by default 
 	//TODO find a solution;
 	private SecretKeySpec aesKey;
 	private String _hexKey;
 	
-	public Encrypter(String key) {
-		init(key);
+	public Encrypter(String key, String iv) {
+		init(key, iv);
+	}
+	public Encrypter(String keyiv) {
+		init(keyiv);
+	}
+	
+	public void init(String keyiv){
+		if (keyiv == null){
+			log.info("null key + iv");
+			return;
+		}
+		if (keyiv.length() < 64) {
+			log.info("keyiv.length < 64 :"+keyiv.length());//we support at least 128bits{
+			return;
+		}
+		init(keyiv.substring(32), keyiv.substring(0, 32));
+	}
+	
+	public byte[] HexToBytes (String hex){
+		if (hex == null){
+			return null;
+		}
+		log.info("hex.length:"+hex.length());
+		int size = hex.length() / 2;
+		if (size == 32){
+			//this is a 256bit key
+		}else if (size == 16){
+			//this is a 128 bits key;
+		}
+		byte [] bytes = new byte[size];
+		for (int i = 0; i < size; i ++){
+			bytes[i] = (byte)Integer.parseInt(hex.substring(i*2, i*2+2), 16);			
+		}
+		return bytes;
 	}
 
-	public void init(String key){
+	public void init(String hexKey, String hexIv){
 		if (_hexKey != null){
-			if (_hexKey.compareTo(key) == 0){
+			if (_hexKey.compareTo(hexKey) == 0){
 				//the key is same;
 				return;
 			}else{
-				_hexKey = key;
+				_hexKey = hexKey;
 			}
 		}else{
-			_hexKey = key;
+			_hexKey = hexKey;
 		}
-		if (key != null && key.length() == 32){
-			for (int i = 0; i < 16; i++){
-				_key[i] = (byte)Integer.parseInt(key.substring(i*2, i*2+2), 16);
-			}
+		if (hexKey == null){
+			return;
 		}
+///*		if (key != null && key.length() == 32){
+//			for (int i = 0; i < 16; i++){
+//				_key[i] = (byte)Integer.parseInt(key.substring(i*2, i*2+2), 16);
+//			}
+//		}*/
+		_key = HexToBytes(hexKey);
 		aesKey = new SecretKeySpec(_key, "AES");
-		// Create an 8-byte initialization vector
-		byte[] iv = new byte[]
-		{
-			0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
-			
-		};
+		// Create an 16-byte initialization vector
+//		byte[] iv = new byte[]
+//		{
+//			0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+//			
+//		};
+		byte [] iv;// = new byte[16];
+		if (hexIv != null){
+			iv = HexToBytes(hexIv);
+		}else{
+			iv = new byte [] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+		}
 		
 		AlgorithmParameterSpec paramSpec = new IvParameterSpec(iv);
 		try
